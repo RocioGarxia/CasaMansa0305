54c0291a23d57c480e591a10a2e207caa2d0fa96
RocioGarxia <rociogarciacrespo@gmail.com>
2021-05-13 23:51:32 -0300

    sumo import material ui core y icons

--- casamansaarte-garcia/src/COMPONENTS/Navbar.js
+++ casamansaarte-garcia/src/COMPONENTS/Navbar.js
@@ -1,5 +1,22 @@
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+
 const Navbar = () => {
-    return <div>Casa Mansa</div>;
-};
+    return (
+    
+<AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
+    );
+
+    };
 
 export default Navbar;

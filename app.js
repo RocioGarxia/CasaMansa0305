4160494b3f364d147384af4c1ffc275cdcb4b4db
RocioGarxia <rociogarciacrespo@gmail.com>
2021-05-13 22:18:32 -0300

    changes to app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ else {
     document.write ("<p>Este email no es confidencial y puede ser reenviado según sea la necesidad.</p>")
 } 
 
-*/
+
 console.time ('tiempo')
 
 let confidencialidad = prompt('Ingrese nivel de confidencialidad del email')
@@ -66,4 +66,87 @@ switch (metodoPago) {
 }
 
 
-console.timeEnd ('tiempo')
+console.timeEnd ('tiempo') */
+/*
+function calculadora(num1, num2, operacion) {
+    switch (operacion) {
+
+        case "+":
+            return num1 + num2;
+            break;
+
+        case "-":
+            return num1 - num2;
+            break;
+
+
+        default:
+            return 0;
+            break;
+
+    }
+}
+
+console.log(calculadora(10, 5, "*"));
+
+
+function sumar(primerNumero, segundoNumero) {
+    return primerNumero + segundoNumero;
+}
+let resultado = sumar(5, 8);
+
+/*
+const suma = (a, b) => { return a + b };
+const resta = (a, b) => a - b;
+console.log(suma(15, 20));
+console.log(resta(20, 5));
+*/
+
+/* Calcular costo total de productos y/o servicios seleccionados por el usuario. */
+
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+
+const iva = x => x * 0.21;
+let precioProducto = 1790;
+let precioOfertaLanzamiento = 48;
+let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioOfertaLanzamiento);
+console.log(nuevoPrecio);
+
+/* Calcular valor final de un producto seleccionado en función de impuestos (IVA) y descuentos (oferta lanzamiento)*/
+
+let resultado = 0
+function sumar(primerNumero, segundoNumero) {
+    resultado = primerNumero + segundoNumero;
+}
+sumar(1500,450);
+console.log(resultado);
+
+/* DESAFIO COMPLEMENTARIO 
+
+let num1 = prompt("ingrese un numero")
+let num2 = prompt("ingrese un numero")
+let op = prompt("Querés quitar este nuevo artículo a tu carrito?")
+
+
+function calculadora(primerNumero, segundoNumero, operacion) {
+    switch (operacion) {
+        case "-":
+            return primerNumero - segundoNumero;
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+
+console.log(num1)
+console.log(num2)
+console.log(op)
+
+console.log(calculadora(num1, num2, op));
+
+let result = (calculadora(num1, num2, op));
+
+document.write (result)
+*/

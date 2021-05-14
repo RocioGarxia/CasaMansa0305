4d8cafc2ead1a4d1d14788c1e28fc047cfbfb58e
RocioGarxia <rociogarciacrespo@gmail.com>
2021-05-14 08:53:55 -0300

    borro clases e importo button

--- casamansaarte-garcia/src/COMPONENTS/Navbar.js
+++ casamansaarte-garcia/src/COMPONENTS/Navbar.js
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, Button, Typography } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 
 const Navbar = () => {
@@ -6,10 +6,10 @@ const Navbar = () => {
     
 <AppBar position="static">
   <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+    <IconButton edge="start" color="inherit" aria-label="menu">
       <MenuIcon />
     </IconButton>
-    <Typography variant="h6" className={classes.title}>
+    <Typography variant="h6">
       News
     </Typography>
     <Button color="inherit">Login</Button>
